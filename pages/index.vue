da76537a7188a4ff6a9ba4518e573f01e46e3ee7
shivamrai83 <rai.shivam@thinksys.com>
2024-08-16 11:49:08 +0530

    Nuxt Shopping App: 1.0.0

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>hi this is Home</p>
+    </div>
+</template>
+
+<script setup>
+const arr = useState('array', () => [1,2,3])
+console.log('arr',arr.value.filter((val)=> val%2===1));
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="grid grid-cols-4 gap-5">
+        <div v-for="p in products">
+            <ProductCard :product="p" />
+        </div>
+    </div>
+    </div>
+</template>
+
+<script setup>
+  const { data: products } = await useFetch('https://fakestoreapi.com/products')
+  console.log('prod', products)
+    definePageMeta({
+    layout: "products",
+  })
+</script>
+
+<style scoped>
+
+</style>
